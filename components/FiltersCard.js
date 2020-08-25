73022ef3c07f6d3748fa845a26e1801d5d120dbc
bitroy <pranoyedu@gmail.com>
2020-08-25 21:40:07 +0530

    styles for filtercard

--- components/FiltersCard.js
+++ components/FiltersCard.js
@@ -7,14 +7,18 @@ const FiltersCard = () => {
     const [launchSuccess, setLaunchSuccess] = useState(null)
     const [landSuccess, setLandSuccess] = useState(null)
 
+	const setLaunchYear = (e) => {
+		if(e.target.type === 'button' && e.target.innerText.trim() !== '') {
+			setYearLaunch(e.target.innerText.trim())
+		}
+	}
+
     const setSuccessfulLaunch = (e) => {
         if(e.target.innerText !== '') {
             if(e.target.innerText.trim() === 'True' || launchSuccess !== true) {
                 setLaunchSuccess(true)
-                missionDispatch(setLaunchSuccessFlag(true))
             } else if(e.target.innerText.trim() === 'False' || launchSuccess !== false) {
                 setLaunchSuccess(false)
-                missionDispatch(setLaunchSuccessFlag(false))
             }
         }
     }
@@ -23,10 +27,8 @@ const FiltersCard = () => {
         if(e.target.innerText !== '') { 
             if(e.target.innerText.trim() === 'True' || landSuccess !== true) {
                 setLandSuccess(true)
-                missionDispatch(setLandSuccessFlag(true))
             } else if(e.target.innerText.trim() === 'False' || landSuccess !== false) {
                 setLandSuccess(false)
-                missionDispatch(setLandSuccessFlag(false))
             }
         }
     }
@@ -36,68 +38,158 @@ const FiltersCard = () => {
 			<div className={styles.filter_div}>
 				<h1>Filters</h1>
 			</div>
-			<div className={styles.filter_div}>
+			<div className={styles.filter_div} onClick={(e) => setLaunchYear(e)}>
 				<h5 className={styles.filter_section_header}>Launch Year</h5>
 				<hr />
 				<Row>
 					<Col className={styles.filter_col}>
-						<Button variant="success">2006</Button>
+						<Button 
+							className={styles.filter_button}	 
+							variant="success" 
+							active={yearLaunch !== null && yearLaunch === '2006' ? true : false}
+						>
+							2006
+						</Button>
 					</Col>
 					<Col className={styles.filter_col}>
-						<Button variant="success">2007</Button>
-					</Col>
-				</Row>
-				<Row>
-					<Col className={styles.filter_col}>
-						<Button variant="success">2008</Button>
-					</Col>
-					<Col className={styles.filter_col}>
-						<Button variant="success">2009</Button>
-					</Col>
-				</Row>
-				<Row>
-					<Col className={styles.filter_col}>
-						<Button variant="success">2010</Button>
-					</Col>
-					<Col className={styles.filter_col}>
-						<Button variant="success">2011</Button>
+						<Button 
+							className={styles.filter_button}	 
+							variant="success" 
+							active={yearLaunch !== null && yearLaunch === '2007' ? true : false}
+						>
+							2007
+						</Button>
 					</Col>
 				</Row>
 				<Row>
 					<Col className={styles.filter_col}>
-						<Button variant="success">2012</Button>
+						<Button 
+							className={styles.filter_button}	 
+							variant="success" 
+							active={yearLaunch !== null && yearLaunch === '2008' ? true : false}
+						>
+							2008
+						</Button>
 					</Col>
 					<Col className={styles.filter_col}>
-						<Button variant="success">2013</Button>
-					</Col>
-				</Row>
-				<Row>
-					<Col className={styles.filter_col}>
-						<Button variant="success">2014</Button>
-					</Col>
-					<Col className={styles.filter_col}>
-						<Button variant="success">2015</Button>
-					</Col>
-				</Row>
-				<Row>
-					<Col className={styles.filter_col}>
-						<Button variant="success">2016</Button>
-					</Col>
-					<Col className={styles.filter_col}>
-						<Button variant="success">2017</Button>
+						<Button 
+							className={styles.filter_button}	 
+							variant="success" 
+							active={yearLaunch !== null && yearLaunch === '2009' ? true : false}
+						>
+							2009
+						</Button>
 					</Col>
 				</Row>
 				<Row>
 					<Col className={styles.filter_col}>
-						<Button variant="success">2018</Button>
+						<Button 
+							className={styles.filter_button}	 
+							variant="success" 
+							active={yearLaunch !== null && yearLaunch === '2010' ? true : false}
+						>
+							2010
+						</Button>
 					</Col>
 					<Col className={styles.filter_col}>
-						<Button variant="success">2019</Button>
+						<Button 
+							className={styles.filter_button}	 
+							variant="success" 
+							active={yearLaunch !== null && yearLaunch === '2011' ? true : false}
+						>
+							2011
+						</Button>
 					</Col>
 				</Row>
 				<Row>
 					<Col className={styles.filter_col}>
-						<Button variant="success">2020</Button>
+						<Button 
+							className={styles.filter_button}	 
+							variant="success" 
+							active={yearLaunch !== null && yearLaunch === '2012' ? true : false}
+						>
+							2012
+						</Button>
+					</Col>
+					<Col className={styles.filter_col}>
+						<Button 
+							className={styles.filter_button}	 
+							variant="success" 
+							active={yearLaunch !== null && yearLaunch === '2013' ? true : false}
+						>
+							2013
+						</Button>
+					</Col>
+				</Row>
+				<Row>
+					<Col className={styles.filter_col}>
+						<Button 
+							className={styles.filter_button}	 
+							variant="success" 
+							active={yearLaunch !== null && yearLaunch === '2014' ? true : false}
+						>
+							2014
+						</Button>
+					</Col>
+					<Col className={styles.filter_col}>
+						<Button 
+							className={styles.filter_button}	 
+							variant="success" 
+							active={yearLaunch !== null && yearLaunch === '2015' ? true : false}
+						>
+							2015
+						</Button>
+					</Col>
+				</Row>
+				<Row>
+					<Col className={styles.filter_col}>
+						<Button 
+							className={styles.filter_button}	 
+							variant="success" 
+							active={yearLaunch !== null && yearLaunch === '2016' ? true : false}
+						>
+							2016
+						</Button>
+					</Col>
+					<Col className={styles.filter_col}>
+						<Button 
+							className={styles.filter_button}	 
+							variant="success" 
+							active={yearLaunch !== null && yearLaunch === '2017' ? true : false}
+						>
+							2017
+						</Button>
+					</Col>
+				</Row>
+				<Row>
+					<Col className={styles.filter_col}>
+						<Button 
+							className={styles.filter_button}	 
+							variant="success" 
+							active={yearLaunch !== null && yearLaunch === '2018' ? true : false}
+						>
+							2018
+						</Button>
+					</Col>
+					<Col className={styles.filter_col}>
+						<Button 
+							className={styles.filter_button}	 
+							variant="success" 
+							active={yearLaunch !== null && yearLaunch === '2019' ? true : false}
+						>
+							2019
+						</Button>
+					</Col>
+				</Row>
+				<Row>
+					<Col className={styles.filter_col}>
+						<Button 
+							className={styles.filter_button}	 
+							variant="success" 
+							active={yearLaunch !== null && yearLaunch === '2020' ? true : false}
+						>
+							2020
+						</Button>
 					</Col>
 					<Col></Col>
 				</Row>
@@ -107,10 +199,22 @@ const FiltersCard = () => {
 				<hr />
 				<Row onClick={(e) => setSuccessfulLaunch(e)}>
 					<Col className={styles.filter_col}>
-						<Button variant="success">True</Button>
+						<Button 
+							className={styles.filter_button} 
+							variant="success" 
+							active={launchSuccess !== null && launchSuccess ? true : false}
+						>
+							True
+						</Button>
 					</Col>
 					<Col className={styles.filter_col}>
-						<Button variant="success">False</Button>
+						<Button 
+							className={styles.filter_button} 
+							variant="success" 
+							active={launchSuccess !== null && !launchSuccess ? true : false}
+						>
+							False
+						</Button>
 					</Col>
 				</Row>
 			</div>
@@ -119,10 +223,22 @@ const FiltersCard = () => {
 				<hr />
 				<Row onClick={(e) => setSuccessfulLand(e)}>
 					<Col className={styles.filter_col}>
-						<Button variant="success">True</Button>
+						<Button 
+							className={styles.filter_button} 
+							variant="success" 
+							active={landSuccess !== null && landSuccess ? true : false}
+						>
+							True
+						</Button>
 					</Col>
 					<Col className={styles.filter_col}>
-						<Button variant="success">False</Button>
+						<Button 
+							className={styles.filter_button} 
+							variant="success" 
+							active={landSuccess !== null && !landSuccess ? true : false}
+						>
+							False
+						</Button>
 					</Col>
 				</Row>
 			</div>
