2ca2cd4414762a88d21b5077ae009f8a4ea2b2bf
bitroy <56228323+bitroy@users.noreply.github.com>
2021-01-31 11:08:05 +0530

    Removed bundleAnalyzer

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
 module.exports = { 
 	target: 'serverless',
-	withBundleAnalyzer({})
 };
